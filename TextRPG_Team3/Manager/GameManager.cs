cfcec68da6073c7b618d24566c9719bef15d0d10
ChanyTheAwesome <89036528+ChanyTheAwesome@users.noreply.github.com>
2025-07-14 17:33:58 +0900

    Add GameManager.cs

---
+++ TextRPG_Team3/Manager/GameManager.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class GameManager
+{
+	private static GameManager instance;
+	public static GameManager Instance => instance ?? (instance = new());
+	public GameManager()
+	{
+	}
+}
