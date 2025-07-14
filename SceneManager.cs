8eded3ce80ae1e297ea9a34397c30cbda488017d
ChanyTheAwesome <89036528+ChanyTheAwesome@users.noreply.github.com>
2025-07-14 17:22:43 +0900

    Add BaseScene,SceneManager

---
+++ SceneManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class SceneManager
+{
+	private static private SceneManager instance = new();
+	public static SceneManager Instance => instance ?? (_instance = new());
+
+	public SceneManager()
+	{
+
+	}
+
+}
--- TextRPG_Team3/Program.cs
+++ TextRPG_Team3/Program.cs
@@ -4,7 +4,39 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            /*Program program = new();
+            program.Init();
+
+            while (true)
+            {
+                program.Render();
+                program.Update();
+            }*/
+            int a = 3;
+            int b = 5;
+            int answer = 0;
+
+            for(int i = 0; i <= a; i++)
+            {
+                answer += b;
+            }
+
+            Console.Write(answer);
+        }
+
+        void Init()
+        {
+
+        }
+
+        void Render()
+        {
+
+        }
+
+        void Update()
+        {
+
         }
     }
 }
