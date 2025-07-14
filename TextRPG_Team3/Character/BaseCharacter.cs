bf0e699780790b7ec1cfbaf7cb2b4c6542448228
muntory <38274669+muntory@users.noreply.github.com>
2025-07-14 17:15:37 +0900

    Create BaseCharacter.cs

---
+++ TextRPG_Team3/Character/BaseCharacter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextRPG_Team3.Character
+{
+    internal class BaseCharacter
+    {
+        public string Name { get; set; }
+
+
+    }
+}
