c62c68f4ade0e955416a1cc7953d3541efabb636
ChanyTheAwesome <89036528+ChanyTheAwesome@users.noreply.github.com>
2025-07-14 17:27:33 +0900

    Add Scene Folder

--- TextRPG_Team3/Program.cs
+++ TextRPG_Team3/Program.cs
@@ -4,24 +4,14 @@
     {
         static void Main(string[] args)
         {
-            /*Program program = new();
+            Program program = new();
             program.Init();
 
             while (true)
             {
                 program.Render();
                 program.Update();
-            }*/
-            int a = 3;
-            int b = 5;
-            int answer = 0;
-
-            for(int i = 0; i <= a; i++)
-            {
-                answer += b;
             }
-
-            Console.Write(answer);
         }
 
         void Init()
