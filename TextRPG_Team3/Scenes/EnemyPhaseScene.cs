9a6a62050b00bfce141c6bf043912d770a11f8e3
ChanyTheAwesome <89036528+ChanyTheAwesome@users.noreply.github.com>
2025-07-15 20:24:51 +0900

    Feat SkipEnemy Method

--- TextRPG_Team3/Scenes/EnemyPhaseScene.cs
+++ TextRPG_Team3/Scenes/EnemyPhaseScene.cs
@@ -14,6 +14,7 @@ namespace TextRPG_Team3.Scenes
         int index = 0;
         public override void Render()
         {
+            SkipEnemy();
             base.Render();
             Console.WriteLine("Battle!!\n");
             int prevHealth = GameManager.Instance.Player.PlayerStat.Health;
@@ -27,7 +28,20 @@ namespace TextRPG_Team3.Scenes
             Console.WriteLine("\n0. 다음");
             index++;
         }
-
+        private void SkipEnemy()
+        {
+            while (currentEnemies.Count>index)
+            {
+                if (currentEnemies[index].CharacterStat.Health <= 0)
+                {
+                    index++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
         public override void SelectMenu(int input)
         {
             Enums.EnemyPhaseMenuE enemyPhaseMenuE = (Enums.EnemyPhaseMenuE)input;
@@ -37,7 +51,6 @@ namespace TextRPG_Team3.Scenes
                 case Enums.EnemyPhaseMenuE.Next:
                     if (index == currentEnemies.Count)
                     {
-                        //SceneManager.Instance.CurrentScene = new PlayerWinScene()?
                         SceneManager.Instance.CurrentScene = new BattleIntroScene(currentEnemies);
                     }
                     else
