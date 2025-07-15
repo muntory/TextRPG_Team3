47c8d7869c69143900395bf4940b4f7b02644ea6
muntory <38274669+muntory@users.noreply.github.com>
2025-07-15 12:23:22 +0900

    [Chore] Rename Miscellaneous-Enums

---
+++ TextRPG_Team3/Miscellaneous/Enums.cs
@@ -0,0 +1,18 @@
+public class Enums
+{
+    public enum IntroMenu
+    {
+        Stat = 1,
+        Battle = 2
+    }
+    
+    public enum StatMenu
+    {
+        Out = 0
+    }
+
+    public enum BattleMenu
+    {
+        Out = 0
+    }
+}
