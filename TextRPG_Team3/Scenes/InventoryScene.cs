2862415f3e8c7d744872c1635bb04a247f602cc0
ChanyTheAwesome <89036528+ChanyTheAwesome@users.noreply.github.com>
2025-07-18 09:53:35 +0900

    Update ItemEquipScene

--- TextRPG_Team3/Scenes/InventoryScene.cs
+++ TextRPG_Team3/Scenes/InventoryScene.cs
@@ -72,7 +72,7 @@ namespace TextRPG_Team3.Scenes
                     SceneManager.Instance.CurrentScene = new IntroScene();
                     break;
                 case Enums.InventoryMenu.Set :
-                    
+                    SceneManager.Instance.CurrentScene = new ItemEquipScene();
                     break;
             }
         }
