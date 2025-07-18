1dbc8645faa317ee612764b644e8424c5e3a74f3
ChanyTheAwesome <89036528+ChanyTheAwesome@users.noreply.github.com>
2025-07-18 14:56:10 +0900

    Update PlayerSaveData.cs

--- TextRPG_Team3/Utils/PlayerSaveData.cs
+++ TextRPG_Team3/Utils/PlayerSaveData.cs
@@ -33,16 +33,16 @@ namespace TextRPG_Team3.Utils
     {
         
     }
-    public class QuestData
+    public class QuestSaveData
     {
         public bool IsCleared {  get; set; }
         public bool IsAccepted {  get; set; }
         public int CurrentAmount { get; set; }
 
-        public QuestData(bool isCleared, bool isAccepted, int currentAmount)
+        public QuestSaveData(bool isCleared, bool isAccepted, int currentAmount)
         {
-            IsCleared =isCleared;
-            IsAccepted =isAccepted;
+            IsCleared = isCleared;
+            IsAccepted = isAccepted;
             CurrentAmount = currentAmount;
         }
     }
