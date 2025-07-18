4f7fa8352de6c57c7e8d23e1f77e8232f396d79a
ChanyTheAwesome <89036528+ChanyTheAwesome@users.noreply.github.com>
2025-07-18 15:09:41 +0900

    Add SavdAndLoad Class & add Save method

--- TextRPG_Team3/Utils/PlayerSaveData.cs
+++ TextRPG_Team3/Utils/PlayerSaveData.cs
@@ -1,9 +1,12 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TextRPG_Team3.Data;
 using TextRPG_Team3.Managers;
+using TextRPG_Team3.Scenes;
 using TextRPG_Team3.Stat;
 
 namespace TextRPG_Team3.Utils
@@ -41,17 +44,51 @@ namespace TextRPG_Team3.Utils
             IsEquipped = isEquipped;
         }
     }
+
     public class QuestSaveData
     {
+        public int QuestID { get; set; }
         public bool IsCleared {  get; set; }
         public bool IsAccepted {  get; set; }
         public int CurrentAmount { get; set; }
 
-        public QuestSaveData(bool isCleared, bool isAccepted, int currentAmount)
+        public QuestSaveData(int questID, bool isCleared, bool isAccepted, int currentAmount)
         {
+            QuestID = questID;
             IsCleared = isCleared;
             IsAccepted = isAccepted;
             CurrentAmount = currentAmount;
+        }
+    }
+
+    public class SaveAndLoad
+    {
+        public void Save()
+        {
+            PlayerSaveData playerData = new PlayerSaveData();
+            List<ItemSaveData> itemData = new List<ItemSaveData>();
+            List<QuestSaveData> questData = new List<QuestSaveData>();
+            SaveQuest(questData);
+        }
+        public void SaveQuest(List<QuestSaveData> questData)
+        {
+            for (int i = 1; i < QuestManager.Instance.QuestDB.Count; i++)
+            {
+                int questID = i;
+                bool isCleared = QuestManager.Instance.QuestDB[i].IsCleared;
+                bool isAccepted = QuestManager.Instance.QuestDB[i].IsAccepted;
+                int currentAmount;
+                if (QuestManager.Instance.QuestDB[i].Goal is KillEnemyQuest killQuest)
+                {
+                    currentAmount = killQuest.CurrentAmount;
+                }
+                else
+                {
+                    currentAmount = -1;
+                }
+                QuestSaveData quest = new QuestSaveData(questID, isCleared, isAccepted, currentAmount);
+                questData.Add(quest);
+            }
         }
     }
 }
